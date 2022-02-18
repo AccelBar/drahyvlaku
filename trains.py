bc058cbe901ad1c7087d68ac7f9dd53466b2794d
Martin Trnka <75213892+AccelBar@users.noreply.github.com>
2022-02-18 11:22:44 +0100

    rozvětveno

--- trains.py
+++ trains.py
@@ -36,21 +36,96 @@ class Funkce1:
         return
 
     def spolecnynasobek():
+        global setkani
         setkani = longertrain * train2time
         setkanimins = setkani // 60
         setkanisecs = setkani % 60
+        setkanimins = setkanimins / 2
+        setkaniminsfull = setkanimins
+        pocetpotkani = setkaniminsfull / timeofridefullmin
+        pocetpotkani = round(pocetpotkani)
         print("Setkají se po uplynutí každého časového úseku")
         print(f"o délce {setkanimins}min a {setkanisecs}s. ")
+        print(f"Potkají se tedy celkem {pocetpotkani}x.")
         return
 
     def jinadoba():
-        prodleva = int(input("O kolik minut vyrazí vlak později?"))
+        global setkani
+        pocetpotkani = 0
+        prodleva = int(input("O kolik minut vyrazí první vlak později?"))
+        longertrain1 = train2time + prodleva
+        setkani = longertrain1 * train2time
+        setkanimins = setkani // 60
+        setkaniminsfull = setkanimins
+        setkanimins = setkanimins / 2
+        setkanisecs = setkani % 60
+        pocetpotkani = setkaniminsfull / timeofridefullmin
+        pocetpotkani = round(pocetpotkani)
+        print("Setkají se po uplynutí každého časového úseku")
+        print(f"o délce {setkanimins}min a {setkanisecs}s. ")
+        print(f"Potkají se tedy celkem {pocetpotkani}x.")
+        return
+
+    def jinadoba1():
+        global setkani
+        global pocetpotkani
+        pocetpotkani = 0
+        prodleva = int(input("O kolik minut vyrazí druhý vlak později?"))
         longertrain1 = longertrain + prodleva
         setkani = longertrain1 * train2time
         setkanimins = setkani // 60
+        setkaniminsfull = setkanimins
+        setkanimins = setkanimins / 2
         setkanisecs = setkani % 60
+        pocetpotkani = setkaniminsfull / timeofridefullmin
+        pocetpotkani = round(pocetpotkani)
         print("Setkají se po uplynutí každého časového úseku")
         print(f"o délce {setkanimins}min a {setkanisecs}s. ")
+        print(f"Potkají se tedy celkem {pocetpotkani}x.")
+        return
+
+    def odkdydokdy():
+        global timeofrideh
+        global timeofridemin
+        global timeofridefullmin
+        global startmin
+        global endmin
+        starthinp = int(input("V kolik hodin vyráží vlaky? "))
+        startmininp = int(input("Kolik minut k tomu? "))
+        endhinp = int(input("Do kolika hodin budou jezdit? "))
+        endmininp = int(input("Kolik minut k tomu? "))
+        startmin = starthinp / 60
+        startmin = startmin + startmininp
+        endmin = endhinp / 60
+        endmin = endmin + endmininp
+        timeofride = startmin - endmin
+        timeofrideh = timeofride / 60
+        timeofridemin = timeofride % 60
+        timeofridefullmin = endmin - startmin
+        return
+
+    def urcitecasy():
+        # pocetpotkani
+        # timeofridefullmin
+        # setkani
+        timehours = timeofridefullmin // 60
+        timemins = timeofridefullmin % 60
+        mezikrok = 0
+        setkanilist = []
+        setkanihourint = 0
+        while pocetpotkani != 0:
+            mezikrok = timemins
+            if mezikrok < 60:
+                setkanihourint = setkanihourint + 1
+                setkanicashour = timehours + setkanihourint
+                setkaniminint = mezikrok - 60
+                setkanifull = (f"{setkanicashour}h a {setkaniminint}min.")
+                setkanilist.append(setkanifull)
+            if mezikrok == 60:
+                setkanihourint = setkanihourint + 1
+                setkanicashour = timehours + setkanihourint
+                setkaniminint = mezikrok - 60
+                setkanifull = (f"{setkanicashour}h a {setkaniminint}min.")
 
 
 print("Vyráží vlaky ze stanice ve stejný moment?")
@@ -60,7 +135,15 @@ if choice < 1:
 if choice > 2:
     Funkce1.neplatnahodnotachoice()
 if choice == 1:
+    Funkce1.odkdydokdy()
     Funkce1.spolecnynasobek()
 elif choice == 2:
-    Funkce1.jinadoba()
-    print("N/A")
+    choice2 = int(input("Vyráží později první nebo druhý vlak?"))
+    if choice2 == 1:
+        Funkce1.odkdydokdy()
+        Funkce1.jinadoba()
+    elif choice2 == 2:
+        Funkce1.odkdydokdy()
+        Funkce1.jinadoba1()
+    else:
+        print("Zadal jsi neplatnou hodnout.")
